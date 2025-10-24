f81358525ba73f0b7aaeacbf0ea75deda426ceff
Ricardo Zúñiga <litosiderito@gmail.com>
2025-10-23 19:52:08 -0600

    Remove unused logo images from the main app component

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,8 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1 className={'border-violet-400 border-5 rounded'}>Vite + React</h1>
