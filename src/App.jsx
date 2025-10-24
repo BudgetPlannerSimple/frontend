39f583c4d275f1dae4e2218bbb87110961ae39d6
Ricardo Zúñiga <litosiderito@gmail.com>
2025-10-23 20:00:09 -0600

    Add Axios for API integration and implement data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,51 @@
-import { useState } from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    const [data, setData] = useState();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
-      </div>
-      <h1 className={'border-violet-400 border-5 rounded'}>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const api = axios.create({
+        baseURL: "",
+        timeout: 10000,
+        headers: {
+            Accept: "application/json",
+        },
+    });
+
+    let a = async () => {
+        const res = await api.get("/");
+        setData(res.data);
+    }
+
+    useEffect(() => {
+        a().then(r => console.log('Done'));
+    },[])
+
+
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                </a>
+                <a href="https://react.dev" target="_blank">
+                </a>
+            </div>
+            <h1 className={'border-violet-400 border-5 rounded'}>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                {data}
+            </p>
+        </>
+    )
 }
 
 export default App
